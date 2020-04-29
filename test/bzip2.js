1dae530d0ec99843851b143fa20be4b82ac1fd1b
coderaiser <coderaiser@cloudcmd.io>
2020-04-29 19:36:49 +0300

    test(bzip2) once

--- test/bzip2.js
+++ test/bzip2.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const {once} = require('events');
 const {tmpdir} = require('os');
 const {
     sep,
@@ -17,65 +18,65 @@ const inly = require('..');
 
 const fixture = join(__dirname, 'fixture');
 
-test('inly: extract: bz2', (t) => {
+test('inly: extract: bz2', async (t) => {
     const to = mkdtempSync(tmpdir() + sep);
     const from = join(fixture, 'fixture.txt.bz2');
     const extracter = inly(from, to);
     
-    extracter.on('end', () => {
-        const pathUnpacked = join(to, 'fixture.txt');
-        const pathFixture = join(fixture, 'fixture.txt');
-        
-        const fileUnpacked = readFileSync(pathUnpacked);
-        const fileFixture = readFileSync(pathFixture);
-        
-        unlinkSync(pathUnpacked);
-        rmdirSync(to);
-        
-        t.deepEqual(fileUnpacked, fileFixture, 'should extract file');
-        t.end();
-    });
+    await once(extracter, 'end');
+    
+    const pathUnpacked = join(to, 'fixture.txt');
+    const pathFixture = join(fixture, 'fixture.txt');
+    
+    const fileUnpacked = readFileSync(pathUnpacked);
+    const fileFixture = readFileSync(pathFixture);
+    
+    unlinkSync(pathUnpacked);
+    rmdirSync(to);
+    
+    t.deepEqual(fileUnpacked, fileFixture, 'should extract file');
+    t.end();
 });
 
-test('inly: extract: bz2: error: empty', (t) => {
+test('inly: extract: bz2: error: empty', async (t) => {
     const error = 'archive is empty';
     const from = join(fixture, 'empty.bz2');
     const extracter = inly(from, 'ss');
     
-    extracter.on('error', (e) => {
-        t.deepEqual(e.message, error, 'should extract file');
-        t.end();
-    });
+    const [e] = await once(extracter, 'error');
+    
+    t.deepEqual(e.message, error, 'should extract file');
+    t.end();
 });
 
-test('inly: extract: bz2: error: not found', (t) => {
+test('inly: extract: bz2: error: not found', async (t) => {
     const error = 'ENOENT: no such file or directory, stat \'not-found.bz2\'';
     const extracter = inly('not-found.bz2', 'ss');
     
-    extracter.on('error', (e) => {
-        t.deepEqual(e.message, error, 'should extract file');
-        t.end();
-    });
+    const [e] = await once(extracter, 'error');
+    
+    t.deepEqual(e.message, error, 'should extract file');
+    t.end();
 });
 
-test('inly: extract: bz2: error: not found', (t) => {
+test('inly: extract: bz2: error: not found', async (t) => {
     const error = 'ENOENT: no such file or directory, stat \'not-found.bz2\'';
     const extracter = inly('not-found.bz2', 'ss');
     
-    extracter.on('error', (e) => {
-        t.deepEqual(e.message, error, 'should extract file');
-        t.end();
-    });
+    const [e] = await once(extracter, 'error');
+    
+    t.deepEqual(e.message, error, 'should extract file');
+    t.end();
 });
 
-test('inly: extract: bz2: error: EACCESS', (t) => {
+test('inly: extract: bz2: error: EACCESS', async (t) => {
     const error = 'ENOENT: no such file or directory, open \'not-found/fixture.txt\'';
     const from = join(fixture, 'fixture.txt.bz2');
     const extracter = inly(from, 'not-found');
     
-    extracter.on('error', (e) => {
-        t.deepEqual(e.message, error, 'should extract file');
-        t.end();
-    });
+    const [e] = await once(extracter, 'error');
+    
+    t.deepEqual(e.message, error, 'should extract file');
+    t.end();
 });
 
